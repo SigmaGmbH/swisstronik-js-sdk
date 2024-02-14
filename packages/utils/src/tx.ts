afde9adcd024cba39f8c7dcc620d538225781e6a
Denis <dev-adv@swisstronik.com>
2024-02-14 16:36:05 +0400

    Added public key functions to swisstronik/utils, bump to 1.2.0

--- packages/utils/src/tx.ts
+++ packages/utils/src/tx.ts
@@ -4,9 +4,39 @@ import {
   USER_KEY_PREFIX,
   decryptECDH,
 } from './encryption'
-import { stringToU8a, hexToU8a, u8aToHex } from './utils'
-import { getNodePublicKey } from './rpc'
-import { randomBytes } from 'tweetnacl'
+import {stringToU8a, hexToU8a, u8aToHex} from './utils'
+import {getNodePublicKey} from './rpc'
+import {randomBytes} from 'tweetnacl'
+
+
+/**
+ * Encrypts provided transaction data using random or provided encryption key using the provided public key
+ * @param nodePublicKey Public key of the node, acquired via eth_getNodePublicKey
+ * @param data Raw transaction `data` field
+ * @param userEncryptionKey Encryption key which will be used as user encryption key for that transaction
+ * @returns Encrypted `data` field for transaction in hex format
+ */
+export function encryptDataFieldWithPublicKey(nodePublicKey: string, data: string | Uint8Array, userEncryptionKey?: Uint8Array): [string, Uint8Array] {
+  // Generate random user encryption key if is not provided
+  userEncryptionKey = userEncryptionKey ?? randomBytes(32);
+
+  // Create encryption key using KDF
+  const encryptionPrivateKey = deriveEncryptionKey(
+    userEncryptionKey,
+    stringToU8a(USER_KEY_PREFIX)
+  );
+  let dataEncoded = typeof data === "string" ? hexToU8a(data) : data;
+  // Encrypt data
+  const encryptionResult = encryptECDH(
+    encryptionPrivateKey,
+    hexToU8a(nodePublicKey),
+    dataEncoded
+  );
+  if (!encryptionResult.result) {
+    throw new Error(`Encryption error. Reason: ${encryptionResult.error}`);
+  }
+  return [u8aToHex(encryptionResult.result), userEncryptionKey];
+}
 
 /**
  * Encrypts provided transaction data using random or provided encryption key
@@ -48,6 +78,33 @@ export async function encryptDataField(
     throw new Error(`Encryption error. Reason: ${encryptionResult.error}`)
   }
   return [u8aToHex(encryptionResult.result), userEncryptionKey]
+}
+
+/**
+ * Decrypts node response (call / transaction result data) using the provided public key
+ * @param nodePublicKey Public key of the node, acquired via eth_getNodePublicKey
+ * @param encryptedResponse Encrypted returned data
+ * @param encryptionKey Key used for encryption
+ * @returns Decrypted result
+ */
+export function decryptNodeResponseWithPublicKey(nodePublicKey: string, encryptedResponse: string | Uint8Array, encryptionKey: Uint8Array): Uint8Array {
+  // Create encryption key using KDF
+  const encryptionPrivateKey = deriveEncryptionKey(
+    encryptionKey,
+    stringToU8a(USER_KEY_PREFIX)
+  );
+
+  let responseEncoded = typeof encryptedResponse === "string" ? hexToU8a(encryptedResponse) : encryptedResponse;
+  const decryptionResult = decryptECDH(
+    encryptionPrivateKey,
+    hexToU8a(nodePublicKey),
+    responseEncoded
+  );
+  if (!decryptionResult.result) {
+    throw new Error(`Decryption error. Reason: ${decryptionResult.error}`);
+  }
+
+  return decryptionResult.result;
 }
 
 /**
