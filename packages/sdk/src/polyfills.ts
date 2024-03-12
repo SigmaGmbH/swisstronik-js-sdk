c680ecff449ca4757f5b65c39f54e8c0c83d5771
Lesther Caballero <lesther.tech@gmail.com>
2024-03-12 15:51:47 +0100

    Fix polyfills and update SDK version (#4)
    * FIX: polyfills now don't need fs module
    
    * sdk@v1.4.0
    
    ---------
    
    Co-authored-by: Lesther Caballero <itslesther@Lesthers-MacBook-Pro.local>

--- packages/sdk/src/polyfills.ts
+++ packages/sdk/src/polyfills.ts
@@ -1,28 +1,22 @@
 import pathModule from "path";
-import fs from "fs";
 import { esmCjsMaps } from "./esm-cjs-maps.js";
-
+// @ts-ignore
+import packageJson from "./package.json" assert { type: "json" };
 
 /* 
 This is a polyfill for the "require" Commonjs function
 This is needed because there are some dependencies that are not ESM compatible
 and we need to use the Commonjs version of the package
 */
-if (fs.existsSync("package.json")) {
-  const packageJson = fs.readFileSync("package.json", "utf-8");
-  const packageJsonObj = JSON.parse(packageJson);
 
-  if (packageJsonObj && packageJsonObj.type === "commonjs") {
-    module.constructor.prototype.require = function (path: string) {
-      var self = this;
+if (packageJson && packageJson.type === "commonjs") {
+  module.constructor.prototype.require = function (path: string) {
+    var self = this;
 
-      if (
-        typeof esmCjsMaps[path] === "string"
-      ) {
-        const dep = pathModule.resolve(__dirname, "deps", esmCjsMaps[path]);
-        return self.constructor._load(dep, self);
-      }
-      return self.constructor._load(path, self);
-    };
-  }
+    if (typeof esmCjsMaps[path] === "string") {
+      const dep = pathModule.resolve(__dirname, "deps", esmCjsMaps[path]);
+      return self.constructor._load(dep, self);
+    }
+    return self.constructor._load(path, self);
+  };
 }
